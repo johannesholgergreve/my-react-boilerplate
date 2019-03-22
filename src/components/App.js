ed1e826632673014665f9c7f54f551c2aaab703d
Johannes Holger Greve <johannesholgergreve@Johanness-MBP-2.localdomain>
2019-03-22 17:33:47 +0100

    Added variables.scss and rename test component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import TestComponent from '../components/TestComponent/index'
 
 class App extends Component {
     render() {
         return (
             <div>
                 <h1>Welcome to My React Boilerplate App</h1>
+                <TestComponent/>
             </div>
         )
     }
