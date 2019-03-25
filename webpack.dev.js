fb6d0eca9b302392eb44252257fffa80759f7bc2
Johannes Holger Greve <johannesholgergreve@Johanness-MBP-2.localdomain>
2019-03-25 16:03:44 +0100

    Added host to webpack.dev file to enable external ip

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
     mode: 'development',
     devServer: {
-        host: 'localhost',
+        host: '0.0.0.0',
         port: 3000,
         open: true,
         hot: true
